65a57d34fa8a2fe2e55247975e47a72b02013ef6
thechinmayrout <thechinmayrout@gmail.com>
2021-05-11 01:15:08 -0400

    Added routes, controllers, middleware,

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,8 @@
 //route-file name is same as controller-file
-const User = require("../models/user")      //recommmnd to name the variable after what u throw in user.js end
+const User = require("../models/user.js")      //recommmnd to name the variable after what u throw in user.js end
 const { check, validationResult } = require("express-validator"); //from express validation documentation
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
 
 exports.signup = (req, res) =>{
 
@@ -33,19 +35,34 @@ exports.signup = (req, res) =>{
 }
 
 exports.signin = (req, res) =>{
-    const {email, password} = req.body;
+    const errors = validationResult(req)
+    const {email, password} = req.body; //destructuring
 
     if(!errors.isEmpty()){
         return res.status(422).json({
                 error: errors.array()[0].msg
             })
     }
-    User.findone({email},(err, user) => {
-        if(err){
-            res.status(400).json({
+    User.findOne({email},(err, user) => {   //findone is mongoose method
+        if(err || !user){   //or adding USER doesn't exist
+            return res.status(400).json({
                 error: "User email doesn't exists"
             })
         }
+
+        if(!user.authenticate(password)){   //from User
+            res.status(401).json({
+                error: "Email and Password do not match"
+            })
+        }
+        //create token
+        const token = jwt.sign({_id: user._id}, process.env.SECRET)
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999})
+
+        //send response to front-end
+        const {_id, name, email, role} = user;
+        return res.json({token, user: {_id, name, email, role}})
 
         
     })
@@ -54,8 +71,37 @@ exports.signin = (req, res) =>{
 
 
 exports.signout = (req, res) =>{           //use /api/signout
+    res.clearCookie("token");   //clear cookie which is token
     res.json({                          //sending json response
-        message: "User Signout! "
+        message: "User Signout Successfully! "
     })
     //res.send("user signout success!")  not using this
+}
+
+
+//protected routes
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+});
+
+
+//custom middlewates
+exports.isAuthenticared = (req, res, next) =>{
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id;  //to check front end && top middleware && profile_id === auth_id
+    if(!checker){
+        return res.status(403).json({
+            error: "ACCESS DENIED"  //if anything is not true; acess id denied
+        })
+    }
+    next()
+}
+
+exports.isAdmin = (req, res, next) =>{
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error: "You are not ADMIN! Access Denied!"
+        });
+    }
+    next()  //super importatn in middleware
 }
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router(); //using express router; get from express routing documentation
-const { signout, signup, signin} = require("../controllers/auth"); //{} contains method of the many methods; ../ means going back from the current directory to parent
+const { signout, signup, signin, isSignedIn} = require("../controllers/auth"); //{} contains method of the many methods; ../ means going back from the current directory to parent
 const { check, validationResult } = require("express-validator"); //from express validation documentation
 
 
@@ -15,8 +15,14 @@ router.post("/signin", [    //post call because taking input from the user
 
     check("email","email is required").isEmail(),
     check("password", "Password field is required").isLength({min: 1})
-])
+],
+signin  //for post request use postman and don't forget to include signin from controller to routes
+)
 
 router.get("/signout", signout);
+
+// router.get("/testroute", isSignedIn, (req, res) => {    //next not required in this middleware as using expressJwt
+//     res.json(req.auth);
+// })
 
 module.exports = router;
